a6d4fb83889aa10f1e01e82cbf7dd4675b86ff09
SiobhanSheehan <siobhanmsheehan@gmail.com>
2018-06-25 17:36:48 -0400

    CSS/style.css

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//const form-contact = document.querySelector('form-contact');
+//form-contact.sytle.display='pink';
+
+//jquery('box').hide();
+//$('.content-form').pink();
+//$('.content-form').purple();
+
+//$(#'email').hide();
+//$('#email').fadeIn(10000);
