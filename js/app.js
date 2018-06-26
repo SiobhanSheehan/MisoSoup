73d68c2f22d09dce759e79533599ca7ee35c2a82
SiobhanSheehan <siobhanmsheehan@gmail.com>
2018-06-26 13:39:20 -0400

    CSS/style.css

--- js/app.js
+++ js/app.js
@@ -7,3 +7,16 @@
 
 //$(#'email').hide();
 //$('#email').fadeIn(10000);
+//you can select the 'email' and run a chain to make the code simpler or you can indent each one
+
+//$('.form-contact').click(function(){
+  //const title = $('#blogTitleInput').val();
+  //const content = $('#blogContentInput').val();
+//  $()
+
+$('.reveal').on('click', 'button', 'function'(){
+ $('.reveal span').show();
+ $('.reveal button').hide();
+});
+const $button =$('<button>Reveal reveal</button>');
+$('reveal').append($button);
