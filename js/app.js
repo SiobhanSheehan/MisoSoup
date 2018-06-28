55c971feb458b7fc8c20deb510a64916a1ae23d8
SiobhanSheehan <siobhanmsheehan@gmail.com>
2018-06-28 16:34:09 -0400

    CSS/style.css

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 // jss for contact form
 
 $(function() {
-  console.log("Hello");
+  console.log("Hello")
 
   $("#submit").click(function () {
     submit();
   });
 });
-
+// on submit - browser will respond with below message
 function submit() {
   alert("thanks for your contact information");
 }
